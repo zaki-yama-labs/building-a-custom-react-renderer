fad0b74622af25755336684d9161c40448e4a463
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-11-29 22:39:14 +0900

    createInstance: copy more props to the element

--- src/ReactDOMMini.js
+++ src/ReactDOMMini.js
@@ -13,8 +13,9 @@ let reconciler = ReactReconciler({
     internalInnstanceHandle,
   ) {
     let el = document.createElement(type);
-    if (props.className) el.className = props.className;
-    if (props.src) el.src = props.src;
+    ['alt', 'className', 'href', 'rel', 'src', 'target'].forEach(k => {
+      if (props[k]) el[k] = props[k];
+    });
     return el;
   },
 
